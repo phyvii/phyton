17ebd40d6ff2cb20908bedd9e3ca193b5498cd11
Krystian Gluszczak <s25586@pjwstk.edu.pl>
2023-03-17 20:56:05 +0100

    zadanie 4

--- archskrypt.py
+++ archskrypt.py
@@ -1,3 +1,33 @@
 import webbrowser
-url = input("podaj strone internetowa ")
-webbrowser.open(url);
+import requests
+
+url = input("Enter the website url: ")
+zapisane = "http://archive.org/wayback/available?url="
+odpowiedz = requests.get(zapisane+url)
+
+if odpowiedz.status_code == 200:
+    odebranee= odpowiedz.json()
+    archiwalne = [odebranee['archived_snapshots']['closest']]
+
+    for i in range(2):
+        czas = archiwalne[i]['timestamp']
+        data = f"{zapisane}{url}&timestamp={czas}"
+        odpowiedz = requests.get(data)
+        data = odpowiedz.json()
+        archiwalne.append(data['archived_snapshots']['closest'])
+
+    i = 1
+    for zapis in archiwalne:
+        archiwalne = zapis['url']
+        time = zapis['timestamp']
+        filename = f"{time}{i}.html"
+        i = i+1
+        odpowiedz = requests.get(archiwalne)
+        content = odpowiedz.content
+        with open(filename, "wb") as f:
+            f.write(content)
+
+        print(filename)
+
+else:
+    print("something went wrong")
